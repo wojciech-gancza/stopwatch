7f47903c0d7cce072072d70f9baac3504768b46b
Wojciech Gancza <wgan@wgan.pl>
2022-12-11 17:09:42 +0100

    Page icon, and response handling - now just receiving of response, but in the future...

--- web_server.py
+++ web_server.py
@@ -8,33 +8,82 @@ import time
 hostName = "localhost"
 serverPort = 8080
 
-class MyServer(BaseHTTPRequestHandler):
+""" Repository of timeseets data """
+class Timeseets:
+
+    def __init__(self):
+        pass
+
+""" Web server serving stopwatch interface and handling data changes """
+class StopwatchServer(BaseHTTPRequestHandler):
+    
     def do_GET(self):
+        if self.path == "/favicon.ico":
+            self._sendFavicon()
+        else: 
+            timesheet_data = StopwatchServer._getLocalFile("example.data.json")
+            self._sendInterface(timesheet_data)
+    
+    def do_POST(self):
+        data = self._getData()
+        print("Recived: " + data)
+        self._sendResponse("OK")
+    
+    def _sendInterface(self, timesheet_data):
+        www_interface = StopwatchServer._getLocalFile("stopwatch.html")
+        json_string_data = StopwatchServer._encode_to_string(timesheet_data)
+        www_page = www_interface.replace(" ***** *** ", json_string_data)
+        self._sendResponse(www_page)
+        
+    def _sendResponse(self, text):    
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        with open("stopwatch.html") as file:
-            www_interface = file.read()
-        with open("example.data.json") as file:
-            timesheet_data = file.read() 
-        www_page = www_interface.replace(" ***** *** ", self.encode_to_string(timesheet_data))
-        self.wfile.write(bytes(www_page, "utf-8"))
+        self.wfile.write(bytes(text, "utf-8"))
     
-    def encode_to_string(self, text):
+    def _sendFavicon(self):
+        icon_data = StopwatchServer._getLocalFile("favicon.ico", True)
+        self.send_response(200)
+        self.send_header("Content-type", "image/x-icon")
+        self.end_headers()
+        self.wfile.write(icon_data)
+        
+    def _getData(self):
+        content_len = int(self.headers.get('Content-Length'))
+        data = self.rfile.read(content_len)
+        return data.decode("Utf-8")
+        
+    @staticmethod 
+    def _getLocalFile(file_name, binary_read = False):
+        try:
+            mode = "r"
+            if binary_read:
+                mode = "rb"
+            with open(file_name, mode) as file:
+                return file.read()
+        except Exception as e:
+            print("File read exception: " + str(e))
+            return None
+    
+    @staticmethod 
+    def _encode_to_string(text):
         text = text.replace("\\", "\\\\")
         text = text.replace("\"", "\\\"")
         text = text.replace("\n", " ")
         text = text.replace("\r", " ")
         return text
+        
+    @staticmethod 
+    def main():
+        webServer = HTTPServer((hostName, serverPort), StopwatchServer)
+        print("Server started http://%s:%s" % (hostName, serverPort))
+        try:
+            webServer.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        webServer.server_close()
+        print("Server stopped.")       
 
+""" Just start web server when module is called as application """
 if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+    StopwatchServer.main()
